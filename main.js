ead0c3ed692b10b40eec86538961fdc6e6e6e45c
g3dms <sirtoto1011@gmail.com>
2026-03-18 12:28:17 +0800

    Modify main.js

--- main.js
+++ main.js
@@ -73,24 +73,3 @@ function init() {
         getCoursePage(courseJSON);
     }
 }
-=======
-    return chrome.runtime.sendMessage(course, function(response) {
-        console.log("Message response: " + response);
-    });
-}
-
-if (document.readyState === "complete") {
-    var courses = document.getElementsByTagName("h2");
-
-    for (var i = 0; i < courses.length; i++) {
-        var courseCode = courses[i].innerText.slice(0,10).replace(/\s/g, '');
-        var url = "https://ust.space/review/" + courseCode;
-        console.log(courseCode + "\'s URL: " + url);
-        courses[i].innerHTML +=  " <a class='rating lower-bar' target='_blank' href=" + url + ">Rating</a>";
-
-        var courseJSON = { code: courseCode };
-        console.log("Getting " + courseJSON.code + " rating");
-        // getCoursePage(courseJSON);
-    }
-}
->>>>>>> 690dcc389276159e831310d03ecaff3799bd9c46
